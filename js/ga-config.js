b4191272c6a1be7beb16ebb1550bd7b4a78b9d1f
enacefio <victorarago@gmail.com>
2025-07-07 10:32:07 +0200

    Añadido script para poner configuración de Google Analytics

---
+++ js/ga-config.js
@@ -0,0 +1,11 @@
+//aquí se puede añadir la configuración de Google Analytics
+
+/*
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-XXXXXXXXXX');
+
+
+*/
