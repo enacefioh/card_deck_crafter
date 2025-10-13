39b4e3c5f38aece63b3fd6d1b1952d3e45a3efa2
enacefio <victorarago@gmail.com>
2025-10-13 17:30:55 +0200

    Actualizado módulo de investigación con nuevas plantillas, por terminar

--- modulos/investigacion/script.js
+++ modulos/investigacion/script.js
@@ -16,14 +16,14 @@ window.Plantillas["investigacion"] = {
 		  height: 88,
 		  html: `
 				<div class='plantilla_apv_standard' style="position:absolute; width:100%; height:100%; top:0px;">
-					<div data-id="nombre_pj" class="texto_linea" style="width:75%; left:4%; top:7px; position:absolute; text-align:center; padding:2px 2px 2px 35px;  border: 3px solid black; border-radius: 10px; font-weight:bold; background-color:#ffffffCC;">Nombre</div>
+					<div data-id="nombre_pj" class="texto_linea" style="width:75%; left:4%; top:7px; position:absolute; text-align:center; padding:4px 2px 2px 35px;  border: 3px solid black; border-radius: 10px; font-weight:bold; background-color:#ffffffCC;">Nombre</div>
 					<div style="width:30px; height:25px; left:5px; top:1%; font-size:15px; position:absolute; text-align:center; text-align:left; padding:10px 0px 0px 5px; border-radius:30px 0px 0px 30px; background:#000; color:#ffffffCC; font-weight:bold;">P</div>
 					<div style="width:24px; height:19px; left:8%; top:1%; font-size:19px; position:absolute; text-align:center; padding:8px 3px 3px 3px; border: 3px solid black; border-radius:30px; background:#ffffff; font-weight:bold;"><span data-id="indice_pj" class="texto_numero">1</span></div>
 					<div class='menu_plantilla' data-id='add_pista' data-plantilla='apv_investigacion_pj_ini' data-modulo='investigacion' style='display:none;'></div>
 					<div class="investigacion_contenedor_pistas" style="width:59mm; bottom:35mm; position:absolute; left:2mm; ">
 						<div class='investigacion_pista' data-indice='1' style='border-top:1px solid gray; border-bottom:1px solid gray;'> 
 							<div class="texto_linea" data-id="icono1" style="font-size:15px; width:3mm; float:left;">ℹ️</div>
-							<div class="texto_linea" data-id="texto1" style="font-size:12px; width:50mm; padding-top:3px; float:right;">Información sobre la persona.</div>
+							<div class="texto_editable" data-id="texto1" style="font-size:12px; width:85%; padding-top:3px; float:right;">Información sobre la persona.</div>
 							<div style='clear:both;'> </div>
 						</div>
 					</div>
@@ -46,14 +46,14 @@ window.Plantillas["investigacion"] = {
 		  height: 88,
 		  html: `
 				<div class='plantilla_apv_standard' style="position:absolute; width:100%; height:100%; top:0px;">
-					<div data-id="nombre_pj" class="texto_linea" style="width:75%; left:4%; top:7px; position:absolute; text-align:center; padding:2px 2px 2px 35px;  border: 3px solid black; border-radius: 10px; font-weight:bold; background-color:#ffffffCC;">Nombre</div>
+					<div data-id="nombre_pj" class="texto_linea" style="width:75%; left:4%; top:7px; position:absolute; text-align:center; padding:4px 2px 2px 35px;  border: 3px solid black; border-radius: 10px; font-weight:bold; background-color:#ffffffCC;">Nombre</div>
 					<div style="width:30px; height:25px; left:5px; top:1%; font-size:15px; position:absolute; text-align:center; text-align:left; padding:10px 0px 0px 5px; border-radius:30px 0px 0px 30px; background:#000; color:#ffffffCC; font-weight:bold;">P</div>
 					<div style="width:24px; height:19px; left:8%; top:1%; font-size:19px; position:absolute; text-align:center; padding:8px 3px 3px 3px; border: 3px solid black; border-radius:30px; background:#ffffff; font-weight:bold;"><span data-id="indice_pj" class="texto_numero">1</span></div>
 					<div class='menu_plantilla' data-id='add_pista' data-plantilla='apv_investigacion_pj_ini' data-modulo='investigacion' style='display:none;'></div>
 					<div class="investigacion_contenedor_pistas" style="width:59mm; bottom:3mm; position:absolute; left:2mm; ">
 						<div class='investigacion_pista' data-indice='1' style='border-top:1px solid gray; border-bottom:1px solid gray;'> 
 							<div class="texto_linea" data-id="icono1" style="font-size:15px; width:3mm; float:left;">ℹ️</div>
-							<div class="texto_linea" data-id="texto1" style="font-size:12px; width:50mm; padding-top:3px; float:right;">Información sobre la persona.</div>
+							<div class="texto_editable" data-id="texto1" style="font-size:12px; width:85%; padding-top:3px; float:right;">Información sobre la persona.</div>
 							<div style='clear:both;'> </div>
 						</div>
 					</div>					
@@ -61,6 +61,89 @@ window.Plantillas["investigacion"] = {
 			`,
 		  cargarOpcionesEnMenu: function (container) { cargarNuevaPista() }
 	  }
+	  ,
+	  {
+		  nombre: 'Localización Inicial',
+		  slug: 'apv_localizacion_ini',
+		  desc: 'Carta de localización inicial',
+		  tags: '',
+		  tipo: 'standard',
+		  width: 63,
+		  height: 88,
+		  html: `
+				<div class='plantilla_apv_standard' style="position:absolute; width:100%; height:100%; top:0px;">
+					<div data-id="nombre_localizacion" class="texto_linea" style="width:75%; left:4%; top:7px; position:absolute; text-align:center; padding:4px 2px 2px 35px;  border: 3px solid black; border-radius: 10px; font-weight:bold; background-color:#ffffffCC;">Lugar</div>
+					<div style="width:30px; height:25px; left:5px; top:1%; font-size:15px; position:absolute; text-align:center; text-align:left; padding:10px 0px 0px 5px; border-radius:30px 0px 0px 30px; background:#000; color:#ffffffCC; font-weight:bold;">L</div>
+					<div style="width:24px; height:19px; left:8%; top:1%; font-size:19px; position:absolute; text-align:center; padding:8px 3px 3px 3px; border: 3px solid black; border-radius:30px; background:#ffffff; font-weight:bold;"><span data-id="indice_localizacion" class="texto_numero">1</span></div>
+					<div class='menu_plantilla' data-id='add_pista' data-plantilla='apv_investigacion_pj_ini' data-modulo='investigacion' style='display:none;'></div>
+					<div class="investigacion_contenedor_pistas" style="width:59mm; bottom:35mm; position:absolute; left:2mm; ">
+						<div class='investigacion_pista' data-indice='1' style='border-top:1px solid gray; border-bottom:1px solid gray;'> 
+							<div class="texto_linea" data-id="icono1" style="font-size:15px; width:3mm; float:left;">ℹ️</div>
+							<div class="texto_editable" data-id="texto1" style="font-size:12px; width:85%; padding-top:3px; float:right;">Información sobre la localización.</div>
+							<div style='clear:both;'> </div>
+						</div>
+					</div>
+					
+					
+					<div data-id="texto" class="texto_editable" style="position:absolute; width:30mm; height:30mm; padding:5px; box-sizing:border-box; top:55mm; left:4mm; font-size:12px; color:#000000;"><b>Tipo: </b>Edificio público<br/><br/><b>Zona: </b>Norte<br/><br/><b>Rol: </b>Escena del crimen</div>
+					<div style="width:25mm; background-position: center center;background-size: cover;border-radius:15px; height: 30mm;margin-left: 35mm;bottom: 10px;position: absolute; background-image: url('modulos/investigacion/localizacion.png');" data-id="foto_localizacion" class="img plantilla_apv_bordes_difuminados"></div>
+					
+					
+				</div>		
+			`,
+		  cargarOpcionesEnMenu: function (container) { cargarNuevaPista() }
+	  },
+	  {
+		  nombre: 'Localización Continuación',
+		  slug: 'apv_investigacion_localizacion_cont',
+		  desc: 'Carta de info adicional de localización APV Investigación',
+		  tags: '',
+		  tipo: 'standard',
+		  width: 63,
+		  height: 88,
+		  html: `
+				<div class='plantilla_apv_standard' style="position:absolute; width:100%; height:100%; top:0px;">
+					<div data-id="nombre_localizacion" class="texto_linea" style="width:75%; left:4%; top:7px; position:absolute; text-align:center; padding:4px 2px 2px 35px;  border: 3px solid black; border-radius: 10px; font-weight:bold; background-color:#ffffffCC;">Lugar</div>
+					<div style="width:30px; height:25px; left:5px; top:1%; font-size:15px; position:absolute; text-align:center; text-align:left; padding:10px 0px 0px 5px; border-radius:30px 0px 0px 30px; background:#000; color:#ffffffCC; font-weight:bold;">L</div>
+					<div style="width:24px; height:19px; left:8%; top:1%; font-size:19px; position:absolute; text-align:center; padding:8px 3px 3px 3px; border: 3px solid black; border-radius:30px; background:#ffffff; font-weight:bold;"><span data-id="indice_localizacion" class="texto_numero">1</span></div>
+					<div class='menu_plantilla' data-id='add_pista' data-plantilla='apv_investigacion_pj_ini' data-modulo='investigacion' style='display:none;'></div>
+					<div class="investigacion_contenedor_pistas" style="width:59mm; bottom:3mm; position:absolute; left:2mm; ">
+						<div class='investigacion_pista' data-indice='1' style='border-top:1px solid gray; border-bottom:1px solid gray;'> 
+							<div class="texto_linea" data-id="icono1" style="font-size:15px; width:3mm; float:left;">ℹ️</div>
+							<div class="texto_editable" data-id="texto1" style="font-size:12px; width:85%; padding-top:3px; float:right;">Información sobre la localización.</div>
+							<div style='clear:both;'> </div>
+						</div>
+					</div>					
+				</div>					
+			`,
+		  cargarOpcionesEnMenu: function (container) { cargarNuevaPista() }
+	  },
+	  {
+		  nombre: 'Indicios Texto',
+		  slug: 'apv_investigacion_indicio_texto',
+		  desc: 'Carta de indicio con solo texto APV Investigación',
+		  tags: '',
+		  tipo: 'standard',
+		  width: 70,
+		  height: 120,
+		  html: `
+				<div class='plantilla_apv_standard' style="position:absolute; width:100%; height:100%; top:0px;">
+					<div data-id="titulo_indicio" class="texto_linea" style="width:75%; left:4%; top:7px; position:absolute; text-align:center; padding:4px 2px 2px 35px;  border: 3px solid black; border-radius: 10px; font-weight:bold; background-color:#ffffffCC;">Indicio</div>
+					<div style="width:30px; height:25px; left:5px; top:1%; font-size:15px; position:absolute; text-align:center; text-align:left; padding:10px 0px 0px 5px; border-radius:30px 0px 0px 30px; background:#000; color:#ffffffCC; font-weight:bold;">I</div>
+					<div style="width:24px; height:19px; left:8%; top:1%; font-size:19px; position:absolute; text-align:center; padding:8px 3px 3px 3px; border: 3px solid black; border-radius:30px; background:#ffffff; font-weight:bold;"><span data-id="indice_indicio" class="texto_numero">1</span></div>
+					<div class='menu_plantilla' data-id='add_pista' data-plantilla='apv_investigacion_pj_ini' data-modulo='investigacion' style='display:none;'></div>
+					<div class="investigacion_contenedor_pistas" style="width:66mm; bottom:3mm; position:absolute; left:2mm; ">
+						<div class='investigacion_pista' data-indice='1' style='border-top:1px solid gray; border-bottom:1px solid gray;'> 
+							<div class="texto_linea" data-id="icono1" style="font-size:15px; width:3mm; float:left;">ℹ️</div>
+							<div class="texto_editable" data-id="texto1" style="font-size:12px; width:85%; padding-top:3px; float:right;">Información sobre el indicio</div>
+							<div style='clear:both;'> </div>
+						</div>
+					</div>					
+				</div>					
+			`,
+		  cargarOpcionesEnMenu: function (container) { cargarNuevaPista() }
+	  }
+	  
 	]
 	
 };
@@ -78,7 +161,7 @@ function cargarNuevaPista(){
 				var num_pistas = parseInt($('.carta_seleccionada * .investigacion_contenedor_pistas').children().first().attr("data-indice"))+1;
 				var html_nueva_pista = `<div class='investigacion_pista eliminable' data-id='eliminar_pista`+num_pistas+`' data-indice='`+num_pistas+`' style='border-top:1px solid gray; border-bottom:1px solid gray;'> 
 							<div class="texto_linea" data-id="icono`+num_pistas+`" style="font-size:15px; width:3mm; float:left;">ℹ️</div>
-							<div class="texto_linea" data-id="texto`+num_pistas+`" style="font-size:12px; width:50mm; padding-top:3px; float:right;">Nueva info</div>
+							<div class="texto_linea" data-id="texto`+num_pistas+`" style="font-size:12px; width:85%; padding-top:3px; float:right;">Nueva info</div>
 							<div style='clear:both;'> </div>
 						</div>`;
 				
